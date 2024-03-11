24cf02f901cd904966bd5ae53893072b6cf3d886
Aarson7 <aarsonsubba02@gmail.com>
2024-03-11 14:02:19 +0545

    Exercises

---
+++ Week01/PersonalDetails.java
@@ -0,0 +1,16 @@
+
+public class PersonalDetails {
+    public static void main(String args[]) {
+
+        String Name = "Aarson Subba";
+        int age = 19;
+        int Phone = 9812345678;
+        String address = "Dharan";
+
+        System.out.println(Name);
+        System.out.println(age);
+        System.out.println(Phone);
+        System.out.println(address);
+
+    }
+}
